2aec1a891f4bbe0bc506666a733d622a4412d2a7
Lars Mensching <133211689+Imiphon@users.noreply.github.com>
2024-06-17 09:06:56 +0200

    add new components and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
@@ -8,12 +8,14 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { Router, NavigationEnd, Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterModule,
     MatToolbarModule,
     MatSidenavModule,
     MatButtonModule,
@@ -28,8 +30,18 @@ import { MatIconModule } from '@angular/material/icon';
 export class AppComponent {
   @ViewChild('drawer') drawer: MatDrawer | undefined;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        console.log('NavigationEnd:', event.urlAfterRedirects);
+      }
+    });
+  }
+
   title = 'simple-crm';
-  events: string[] = [];
   opened: boolean | undefined;
-  //shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
+
+  toggleDrawer() {
+    this.drawer?.toggle();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+import { InfoComponent } from './info/info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        children: [
+
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'user', component: UserComponent },
+            { path: 'info', component: InfoComponent },
+            { path: '', redirectTo: 'info', pathMatch: 'full' }, // Default route
+        ]
+    }
+];
