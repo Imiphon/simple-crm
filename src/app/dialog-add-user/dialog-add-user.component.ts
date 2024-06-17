9342a2532f0bfc3f88ba0f35d3de02eda5f58803
Lars Mensching <133211689+Imiphon@users.noreply.github.com>
2024-06-17 11:33:21 +0200

    work on button box shadow in user-component

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-dialog-add-user',
-  standalone: true,
-  imports: [],
+  selector: 'dialog-add-user',
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrls: ['./dialog-add-user.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule]
 })
 export class DialogAddUserComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DialogAddUserComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import {MatDialog} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
@@ -8,16 +9,24 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
   standalone: true,
   imports: [
     MatIconModule,
+    MatButtonModule,
+    MatDialogModule
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
-  constructor(public dialog: MatDialog) { }
- 
+  readonly dialog = inject(MatDialog);
 
-openDialog(){
-console.log('openDialog starts');
-this.dialog.open(DialogAddUserComponent);
-}
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddUserComponent, {
+      width: '300px',
+      data: { anyData: 'example' } // Hier können Sie beliebige Daten übergeben
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
