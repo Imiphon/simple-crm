e19ed8feed6e0a106893d6ffeebe6215773c02f5
Lars Mensching <133211689+Imiphon@users.noreply.github.com>
2024-08-24 11:03:26 -0400

    finish 24 aug

--- src/app/dialog-edit-address/dialog-edit-address.component.ts
+++ src/app/dialog-edit-address/dialog-edit-address.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectorRef, Component, Inject, OnInit, signal } from '@angular/c
 import { UserService } from '../services/user.service';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef,  } from '@angular/material/dialog';
+import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef, } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,7 +10,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CommonModule } from '@angular/common';
-//import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 
 @Component({
@@ -31,7 +30,7 @@ import { User } from '../../models/user.class';
   templateUrl: './dialog-edit-address.component.html',
   styleUrl: './dialog-edit-address.component.scss'
 })
-export class DialogEditAddressComponent implements OnInit{
+export class DialogEditAddressComponent implements OnInit {
   form: FormGroup
   loading: boolean = false;
   errorMessage = signal('');
@@ -46,7 +45,6 @@ export class DialogEditAddressComponent implements OnInit{
     private cdr: ChangeDetectorRef
 
   ) {
-
     this.form = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -59,12 +57,7 @@ export class DialogEditAddressComponent implements OnInit{
       birthday: ['', Validators.required],
       customIdName: ['', Validators.required]
     });
-  
-  //  merge(this.email.statusChanges, this.email.valueChanges)
-  //    .pipe(takeUntilDestroyed())
-  //    .subscribe(() => this.updateErrorMessage());
-
-    this.dialogRef.updateSize('auto'); 
+    this.dialogRef.updateSize('auto');
   }
 
   ngOnInit() {
@@ -72,7 +65,6 @@ export class DialogEditAddressComponent implements OnInit{
       this.userService.getUser(this.data.customIdName).subscribe(user => {
         this.user = user;
         this.fillForm(user);
-        console.log('User data:', user);
         this.cdr.markForCheck();
       });
     } else {
@@ -97,40 +89,44 @@ export class DialogEditAddressComponent implements OnInit{
     });
   }
 
-//  updateErrorMessage() {
-//    if (this.email.hasError('required')) {
-//      this.errorMessage.set('You must enter a value');
-//    } else if (this.email.hasError('email')) {
-//      this.errorMessage.set('Not a valid email');
-//    } else {
-//      this.errorMessage.set('');
-//    }
-//  }
+  //  updateErrorMessage() {
+  //    if (this.email.hasError('required')) {
+  //      this.errorMessage.set('You must enter a value');
+  //    } else if (this.email.hasError('email')) {
+  //      this.errorMessage.set('Not a valid email');
+  //    } else {
+  //      this.errorMessage.set('');
+  //    }
+  //  }
 
-  saveUser() {
-    debugger
-    if (this.form.invalid) {
-      this.form.markAllAsTouched();
-      return;
-    }
-    this.loading = true;
+editUser(){
+  console.log('editUser() starts');
+  
+}
 
-    // Copy form values to user object
-    Object.assign(this.user, this.form.value);
-
-    const birthDateValue = this.form.get('birthday')?.value;
-    if (birthDateValue instanceof Date) {
-      this.user.birthday = birthDateValue.getTime();  // Convert to Unix timestamp
-    }
-
-    this.userService.addUser(this.user).then(() => {
-      this.loading = false;
-      this.dialogRef.close();
-    }).catch(error => {
-      this.loading = false;
-      console.error('error with adding user: ', error);
-    });
-  }
+  // saveUser() {
+  //   if (this.form.invalid) {
+  //     this.form.markAllAsTouched();
+  //     return;
+  //   }
+  //   this.loading = true;
+// 
+  //   // Copy form values to user object
+  //   Object.assign(this.user, this.form.value);
+// 
+  //   const birthDateValue = this.form.get('birthday')?.value;
+  //   if (birthDateValue instanceof Date) {
+  //     this.user.birthday = birthDateValue.getTime();  // Convert to Unix timestamp
+  //   }
+// 
+  //   this.userService.addUser(this.user).then(() => {
+  //     this.loading = false;
+  //     this.dialogRef.close();
+  //   }).catch(error => {
+  //     this.loading = false;
+  //     console.error('error with adding user: ', error);
+  //   });
+  // }
 
   onNoClick(): void {
     this.dialogRef.close();
